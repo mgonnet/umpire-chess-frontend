7b414f26e32c47ef2df016edeadb9f683395fd8c
mgonnet <gonnet.matias@gmail.com>
2019-05-27 18:46:48 -0300

    change to wss

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App'
 import { UmpireClient } from '@mgonnet/umpire-client'
 
-let client = UmpireClient({url: 'ws://umpire-chess.herokuapp.com/', WSConstructor: WebSocket})
+let client = UmpireClient({url: 'wss://umpire-chess.herokuapp.com/', WSConstructor: WebSocket})
 
 ReactDOM.render(<App client={client}/>, document.getElementById('root'));
